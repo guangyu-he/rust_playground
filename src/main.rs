f8b13ebd9dd97d22668918ee23d6bb1e515a411b
Guangyu He <fredhgy2010@gmail.com>
2022-12-17 23:12:03 +0100

    half coding

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,37 @@
-use itertools::Itertools;
-
-fn main() -> () {
-    let nums = ["b", "a", "b", "c"];
-    let rem_du_list: Vec<_>;
-    rem_du_list = duplicate_item_in_array(nums.to_vec());
-    println!("{:?}", rem_du_list);
+struct Card {
+    rank: String,
+    suit: String,
 }
 
-fn duplicate_item_in_array(input: Vec<&str>) -> Vec<&str> {
-    let output: Vec<_> = input.into_iter().unique().collect();
-    output
+struct FrenchDeck {
+    ranks: Vec<String>,
+    suits: Vec<String>,
+    cards: Vec<Card>,
+}
+
+impl FrenchDeck {
+    fn new() -> FrenchDeck {
+        let ranks_list = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"].map(String::from).to_vec();
+        let suits_list = ["♣", "♠", "♥", "♦"].map(String::from).to_vec();
+        let mut cards_list = vec![];
+        for each_suit in suits_list {
+            for each_rank in ranks_list {
+                cards_list.push(Card {
+                    rank: each_rank.to_string(),
+                    suit: each_suit.to_string(),
+                });
+            }
+        }
+        FrenchDeck {
+            ranks: ranks_list,
+            suits: suits_list,
+            cards: cards_list,
+        }
+    }
+}
+
+fn main() {
+    let a_deck = FrenchDeck::new();
+    let a_len = a_deck.cards.len();
+    println!("Length of deck: {}", a_len);
 }
